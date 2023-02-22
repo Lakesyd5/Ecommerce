90a49eb699b63eeeb2a76bcb8fa9720dd6ae467f
Developer Adele <adeleoje@gmail.com>
2023-02-22 21:58:55 +0100

    added a payment method

--- app.js
+++ app.js
@@ -178,9 +178,6 @@ function handleClick(id) {
 
 function viewCart() {
     storedCart = JSON.parse(localStorage.getItem("cart"));
-    let totalPriceDisplay = document.getElementById("total");
-    let totalprice = 0;
-
 
     // If the cart is empty, display a message to the user
     if (!storedCart || storedCart.length == 0) {
@@ -202,11 +199,20 @@ function viewCart() {
              <button onclick="removeItem(${index})" class="del">Remove</i></button>
            </div>
         `
-            totalprice += element.price;
         }
-        totalPriceDisplay.innerHTML = `$${totalprice.toFixed(2)}`
+        
     }
 }
+
+// Total
+let totalDisplay = document.getElementById("total");
+let totalPrice = 0;
+storedCart = JSON.parse(localStorage.getItem("cart"))
+storedCart.forEach(element => {
+    totalPrice += element.price;
+    totalDisplay.innerHTML = `$${totalPrice.toFixed(2)}`
+});
+
 
 
 function removeItem(index) {
@@ -240,4 +246,29 @@ if (!storedCart || storedCart.length == 0) {
     badge.textContent = '0';
 } else {
     badge.textContent = storedCart.length.toString();
+}
+
+function makePayment() {
+    FlutterwaveCheckout({
+        public_key: "FLWPUBK_TEST-0c58230ed063cb8f5f84cfcd0050be7d-X",
+        tx_ref: "titanic-48981487343MDI0NzMx",
+        amount: totalPrice,
+        currency: "USD",
+        payment_options: "card, mobilemoneyghana, ussd",
+        redirect_url: "https://glaciers.titanic.com/handle-flutterwave-payment",
+        meta: {
+            consumer_id: 23,
+            consumer_mac: "92a3-912ba-1192a",
+        },
+        customer: {
+            email: "rose@unsinkableship.com",
+            phone_number: "08102909304",
+            name: "Rose DeWitt Bukater",
+        },
+        customizations: {
+            title: "The Titanic Store",
+            description: "Payment for an awesome cruise",
+            logo: "https://www.logolynx.com/images/logolynx/22/2239ca38f5505fbfce7e55bbc0604386.jpeg",
+        },
+    });
 }
